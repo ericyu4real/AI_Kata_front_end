96e5256bb92e904cf96bb5364b21b8dd58e8f72a
Your Name <you@example.com>
2024-04-04 22:42:43 -0400

    netlify adjustment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist', // It should point to a 'dist' folder at the root
+  },
   plugins: [react()],
 })
